3cdcc6df196f2dfcca120142ef61c81726cc8f3f
Mario Castro Squella <mario@theagilemonkeys.com>
2020-08-20 16:05:32 -0400

    Added files for an empty iOS project

---
+++ iOS/src/AmplifyCognitoAuthDemo/AmplifyCognitoAuthDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AmplifyCognitoAuthDemo
+//
+//  Created by Mario Castro Squella on 20-08-20.
+//  Copyright © 2020 Technology-Science. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
